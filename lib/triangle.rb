fad69f751d7a42d6c4436a65529a6982d15b6e87
Billy Ferrara <willfira717@gmail.com>
2020-06-10 20:05:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,6 +21,10 @@ class Triangle
     if side_1 == side_2 && side_2 == side_3 && side_1 == side_3
       :equilateral
     elsif side_1 != side_2 && side_2 != side_3 && side_1 != side_3
+      :scalene
+    else
+      :isosceles
+    end 
 
   end
 
